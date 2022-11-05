53b81ca7bef451c7f5af986e1f8e294a13f23b96
Mohaimenul <mahadihasan1603@gmail.com>
2022-11-05 19:10:36 +0600

    Interface Added

---
+++ interfaces/Hospital.java
@@ -0,0 +1,9 @@
+package interfaces;
+import classes.doctor;
+public interface Hospital{
+
+   public void new_doctor();
+   
+
+
+}
---
+++ interfaces/Utilities.java
@@ -0,0 +1,7 @@
+package interfaces;
+import classes.medical;
+public interface Utilities{
+  
+  public void new_medi();
+
+}
